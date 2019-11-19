84e2e4cc5de6ca87232ce02f799ccf6610cd8923
JeanFraga <JeanFraga@users.noreply.github.com>
2019-11-19 13:33:03 -0500

    make it better

--- CANNABIS_API/models.py
+++ CANNABIS_API/models.py
@@ -9,13 +9,7 @@ from flask_marshmallow import Marshmallow
 DB = SQLAlchemy()
 
 class Records(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
-    strain = DB.Column(DB.String(25))
-    strain_type = DB.Column(DB.String(6))
-    rating = DB.Column(DB.Float)
-    effects = DB.Column(DB.String(100))
-    flavor = DB.Column(DB.String(100))
-    description = DB.Column(DB.String(2000))
+    id = DB.Column(DB.BigInteger, primary_key=True)
 
     def __repr__(self):
-        return '<Strain {}>'.format(self.strain)
+        return '<id is {}>'.format(self.id)
