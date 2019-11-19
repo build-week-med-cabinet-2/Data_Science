091c91125945a331fbe39d6276db7809e5eb0000
JeanFraga <JeanFraga@users.noreply.github.com>
2019-11-18 21:13:59 -0500

    added schema for API

--- CANNABIS_API/hello_test.py
+++ CANNABIS_API/hello_test.py
@@ -7,9 +7,9 @@ from flask_restful import Resource
 
 
 class HelloWorld(Resource):
-	def get(self):
-		return {'about': 'Hello API!'}
+    def get(self):
+        return {'about': 'Hello API!'}
 
-	def post(self):
-		some_json = request.get_json()
-		return {'you sent': some_json}, 201
+    def post(self):
+        some_json = request.get_json()
+        return {'you sent': some_json}, 201
